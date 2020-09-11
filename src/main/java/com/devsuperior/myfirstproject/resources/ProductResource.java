2f7937fda1e52ae28b0f9cbdc3d711dea8fd12d8
campigoto <campigoto@hotmail.com>
2020-09-10 22:38:06 -0300

    H2 Database

--- src/main/java/com/devsuperior/myfirstproject/resources/ProductResource.java
+++ src/main/java/com/devsuperior/myfirstproject/resources/ProductResource.java
@@ -27,7 +27,7 @@ public class ProductResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Long id) {
-		Product pro = productRepository.findById(id);
+		Product pro = productRepository.findById(id).get();
 		return ResponseEntity.ok().body(pro);
 	}
 
